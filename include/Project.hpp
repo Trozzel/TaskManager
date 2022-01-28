838041bfc546599c92e6d7c5716439b3bfec3947
George Ford <george.ford@icloud.com>
2022-01-28 15:31:09 -0500

    Populating context

--- include/Project.hpp
+++ include/Project.hpp
@@ -6,16 +6,34 @@
 #define GTD_PROJECT_HPP
 
 #include <string>
+#include <list>
 
 #include "CTime.hpp"
+#include "ProjectType.hpp"
+#include "Status.hpp"
+#include "Context.hpp"
 
 namespace gtd {
 
 class Project {
 private:
-    std::string _projectName;
+    std::string _projectName{};
+    std::string _note{};
+    Status      _status           { Status::Active };
+    ProjectType _projectType      { ProjectType::Parallel };
+    bool        _completeWithLast { false };
+
+    std::list<const Context*> _contexts;
+
+    dt::CTime _created{};
+    dt::CTime _modified{};
+    dt::CTime _deferred{};
+    dt::CTime _due{};
+    dt::CTime _completed{};
+    dt::CTime _dropped;
 
 public:
+
 };
 
 } // namespace gtd
---
+++ include/ProjectType.hpp
@@ -0,0 +1,12 @@
+//
+// Created by George Ford on 1/28/22.
+//
+
+#ifndef GTD_PROJECTTYPE_HPP
+#define GTD_PROJECTTYPE_HPP
+
+enum class ProjectType {
+    Parallel, Sequential, SingleActions
+};
+
+#endif //GTD_PROJECTTYPE_HPP
